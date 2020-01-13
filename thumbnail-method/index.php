b5807542bac4bd584b47a65459bdf134d5a8f603
IGGMFoundation <59816045+IGGMFoundation@users.noreply.github.com>
2020-01-13 15:32:53 +0800

    Add files via upload

---
+++ thumbnail-method/index.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Website bootstrap kedua (buat ulang)</title>
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="fontawesome/css/all.css">
+</head>
+<body data-spy="scroll" data-target=".navbar" data-offset="80">
+	<header class="header">
+		<nav class="navbar fixed-top navbar-expand-sm navbar-dark bg-success">
+			 <a class="navbar-brand" href="#"><img src="title/gizmobali.png" style="width: 120px;height: 40px;"></a>
+			 <div class="collapse navbar-collapse" id="navbarNav">
+			    <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link" href="#"><i class="fas fa-home"></i></a></li>
+				    <li class="nav-item"><a class="nav-link" href="#gotoabout"><i class="fas fa-align-justify"></i></a></li>
+                    <li class="nav-item"><a class="nav-link" href="#articles"><i class="fas fa-palette"></i></a></li>
+                    <li class="nav-item"><a class="nav-link" href="#gotolicense"><i class="fas fa-pencil-alt"></i></a></li>
+                    <li class="nav-item"><a class="nav-link" href="#contactme"><i class="fas fa-address-book"></i></a></li>
+			    </ul>
+			 </div>
+		</nav>
+		<img class="banner" src="banner1.png" style="width: 100%;height: 50%;">
+	</header>
+		
+	<section id="gotoabout">
+		<div class="container">
+        <br>
+        <br>
+        <br>
+        <h1 style="text-align: center;"><img src="title/tentang-kami.png"></h1>
+        <div class="row">
+                <div class="col-md-4">
+					<div class="thumbnail">
+                        <img src="gizbali1.jpg" style="width: 345px;height: 150px;">
+				        <div class="caption" style="font-family: 'Segoe UI';text-align: justify;font-size: 22px;">SEBUAH WEBSITE YANG SERVISNYA 100 KALI. YANG TENTUNYA FOKUS KE TEKNOLOGI, VIDEO GAME, SEPUTAR FILM TERANYAR DAN INFO-INFO BERFAEDAH DAN NON-BERFAEDAH LAINNYA DARI ARI OCTALLITY!!!</div>
+					</div>
+				</div>
+                <div class="col-md-4">
+					<div class="thumbnail">
+                        <img src="fa5.png" style="width: 345px;height: 150px;">
+				        <div class="caption" style="font-family: 'Segoe UI';font-size: 22px;">WEB FRAMEWORK JAUH LEBIH SEDERHANA DARI SEBELUMNYA, KARENA WEBSITE INI MENGGUNAKAN BOOTSTRAP YANG SERBA PALING BARU, ICON FONT AWESOME YANG LEBIH SEDERHANA.</div>
+					</div>
+				</div>
+                <div class="col-md-4">
+					<div class="thumbnail">
+                        <img src="bootstrap-wallpaper.png" style="width: 345px;height: 150px;">
+				        <div class="caption" style="font-family: 'Segoe UI';font-size: 22px;">PENEMU WEBSITE INI, YA JELASLAH ORANG YANG SUKSES, PEMBERANI, SETENGAH IDIOT, GAGAH, DAN PEMBERANI YAITU I GUSTI BAGUS ARIGUNA "OCTALLITY" WICAKSANA.</div>
+					</div>
+				</div>
+        </div>
+    </section>
+        <br>
+        <br>
+        <br>
+    <section id="articles">
+        <div class="container-fluid">
+            <br><br><br><br>
+            <h1 style="text-align: center;"><img src="title/artikel-terkini.png"></h1>
+            <div class="row">
+				<div class="col-md-4">
+					<div class="thumbnail">
+						<a href="#">
+							<img src="cplusplus.png" style="width: 400px;height: 200px;">
+							<div class="caption" style="font-family: 'Segoe UI';font-size: 22px;">5 Hal-hal yang harus diperhatikan saat membuat program C++</div>
+						</a>
+					</div>
+				</div>
+                <div class="col-md-4">
+                    <div class="thumbnail">
+                        <a href="#">
+                            <img src="javaskrip.jpeg" style="width: 400px;height: 200px;">
+                            <div class="caption" style="font-family: 'Segoe UI';font-size: 22px;">Mengenal JavaScript, cara mengutak-atik Web yang ampuh.</div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="thumbnail">
+                        <a href="#">
+                            <img src="inisass.jpg" style="width: 400px;height: 200px;">
+                            <div class="caption" style="font-family: 'Segoe UI';font-size: 22px;">Sass, Cara Praktis untuk membuat stylesheet web. Begini Penjelasannya</div>
+                        </a>
+                    </div>
+                </div>
+			</div>
+            <div class="row">
+                <div class="col-md-4">
+					<div class="thumbnail">
+						<a href="#">
+							<img src="never-settle1.jpg" style="width: 400px;height: 200px;">
+							<div class="caption" style="font-family: 'Segoe UI';font-size: 22px;">Sejarah terbentuk dan berkembangnya Oneplus di seluruh dunia. Dari kolaborasi CyanogenMod, berdirinya OxygenOS, dan lain sebagainya.</div>
+						</a>
+					</div>
+				</div>
+                <div class="col-md-4">
+					<div class="thumbnail">
+						<a href="#">
+							<img src="android-v10.jpg" style="width: 400px;height: 200px;">
+							<div class="caption" style="font-family: 'Segoe UI';font-size: 22px;">Android 10.0 Q Kini Menyebar ke Vendor Lain, Google Pixel sudah masuk ke tahap Beta. Apakah ini fakta rumor atau meyakinkan, ini penjelasannya!</div>
+						</a>
+					</div>
+				</div>
+                <div class="col-md-4">
+					<div class="thumbnail">
+						<a href="#">
+							<img src="samsung_galaxy_a9_a7_2018_1542791484632.jpg" style="width: 400px;height: 200px;">
+							<div class="caption" style="font-family: 'Segoe UI';font-size: 22px;">Samsung Galaxy A7 dan A9 mendapatkan pembaruan One UI berbasis Android Pie. Update sekarang untuk mendapatkan fitur-fitur menarik lainnya.</div>
+						</a>
+					</div>
+				</div>
+            </div>
+        </div>
+    </section>
+    <section id="gotolicense">
+        <div class="container-fluid">
+            <br>
+            <br>
+            <br>
+            <h1 style="text-align: center;"><img src="title/lisensi.png"></h1>
+            <br>
+            <br>
+            <div class="row">
+                <div class="col-md-4">
+					<div class="thumbnail">
+                        <img src="bootstrap-wallpaper.png" style="width: 400px;height: 200px;">
+				        <div class="caption" style="font-family: 'Segoe UI';">BOOTSTRAP TERBARU 2019, TOTAL MELAYANI WEB DESIGN YANG MENARIK, KAMI AKAN IMPROVISASI WEBSITE INI SEBAGAIMANA DIATAS. INOVASI MEMBAHANA DARI OCTALLITY.</div>
+					</div>
+				</div>
+                <div class="col-md-4">
+					<div class="thumbnail">
+                        <img src="fa5.png" style="width: 400px;height: 200px;">
+				        <div class="caption" style="font-family: 'Segoe UI';">FONT AWESOME 5, ICON SVG PENUNJANG KESEDERHANAAN. MEMBERIKAN YANG TERBAIK UNTUK BATANG NAVIGASI. KAMI SELALU MENGGUNAKAN INI DEMI GIZMOBALI, WEBSITE IT PERTAMA DI PROVINSI BALI.</div>
+					</div>
+				</div>
+                <div class="col-md-4">
+					<div class="thumbnail">
+                        <img src="jquery-js.png" style="width: 400px;height: 200px;">
+				        <div class="caption" style="font-family: 'Segoe UI';">JAVASCRIPT-JQUERY-BOOTSTRAP.JS, BAHASA PEMROGRAMAN AKAN MENGUBAH WEBSITE ANDA LEBIH FUTURISTIK, AKTUAL, TERORGANISIR, DAN LAIN SEBAGAINYA. KAMI AKAN SELALU MENGEMBANGKAN INI SAMPAI KAPANPUN DIMANAPUN.</div>
+					</div>
+				</div>
+            </div>
+        </div>
+    </section>
+    <section id="contactme">
+        <div class="container-fluid">
+            <br><br><br><br><br>
+            <h1 style="text-align: center"><img src="title/hubungi-kami.png"></h1>
+            <h2 style="text-align: center;">
+                <a href="https://www.instagram.com/arigunawicaksanaaa"><button type="button" class="btn btn-dark btn-lg" style="border-radius: 50%;"><i class="fab fa-instagram"></i></button></a>
+                <button type="button" class="btn btn-dark btn-lg" style="border-radius: 50%;" href=""><i class="fab fa-github"></i></button>
+                <button type="button" class="btn btn-dark btn-lg" style="border-radius: 50%;" href=""><i class="fab fa-line"></i></button>
+            </h2>
+            <br>
+            <div style="text-align: center;">
+                <form>
+                    <div class="form-group">
+                        <label for="email">Email address:</label>
+                        <input type="email" class="form-control" id="email">
+                    </div>
+                    <div class="form-group">
+                        <label for="username">Username : </label>
+                        <input type="username" class="form-control" id="username">
+                    </div>
+                    <div class="form-group">
+                        <label for="username">Description to OP : </label>
+                        <textarea id="input" class="form-control" maxlength="450" rows="4"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+            <br>
+        </div>    
+    </section>
+        <nav class="navbar navbar-expand-sm bg-dark navbar-light sticky-top">
+            <h1 style="color: azure;">I Gusti Bagus Ariguna Wicaksana (X RPL 3 / 8)</h1>
+        </nav>
+</body>
+</html>
